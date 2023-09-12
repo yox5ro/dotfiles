74cab1565ab000a988f6105a682fa5f1751d078f
Yoshihiro Koyama <h2so4@mineo.jp>
2023-09-13 01:12:39 +0900

    natural file tree view

--- nvim/lua/plugins/fern.lua
+++ nvim/lua/plugins/fern.lua
@@ -1,30 +1,62 @@
-function get_git_root_dir_or_cwd()
-	-- git リポジトリで nvim が開かれたならリポジトリの root ディレクトリを、そうでないならカレントディレクトリを返す
-	command = "git rev-parse --show-toplevel"
-	is_git_repo, git_root_dir = pcall(vim.fn.system, command)
+local function get_git_root_dir()
+	local command = "git rev-parse --show-toplevel"
+	local is_git_repo, git_root_dir = pcall(vim.fn.system, command)
 	if is_git_repo then
-		return string.gsub(git_root_dir, "\n", "")
+		git_root_dir = string.gsub(git_root_dir, "\n", "")
+		if vim.fn.isdirectory(git_root_dir) ~= 1 then
+			return nil
+		end
+		vim.cmd.cd(git_root_dir)
+		return git_root_dir
 	end
-	return vim.fn.getcwd()
+	return nil
 end
 
-function open_fern()
-	wd = get_git_root_dir_or_cwd()
-	current_file = vim.api.nvim_buf_get_name(0)
-	command = string.format("Fern %s -drawer -reveal=%s", wd, current_file) 
+local function cd_and_get_dir_info(file_or_dir)
+	-- if opened file, get parent dir of the file and cd to the dir.
+	-- if opened dir, cd to the dir.
+	-- returns absolute path to working dir and reveal target of file tree.
+	local wd = file_or_dir
+	local reveal_target = file_or_dir
+	local is_directory = vim.fn.isdirectory(file_or_dir) == 1
+	if not is_directory then
+		wd = vim.fn.fnamemodify(file_or_dir, ":h")
+	end
+	vim.cmd.cd(wd)
+	return wd, reveal_target, is_directory
+end
+
+local function calc_relative_path(base_path, target_path)
+	-- calculate relative path to target_path from base_path
+	-- base_path and target_path are both given in absolute path
+	return vim.fn.fnamemodify(target_path, string.format(":s?%s/??", base_path))
+end
+
+local function open_fern(data)
+	local wd, reveal_target, is_target_directory = cd_and_get_dir_info(data.file)
+	local git_root_dir = get_git_root_dir()
+	if git_root_dir then
+		wd = git_root_dir
+	end
+	local relative_reveal_target = calc_relative_path(wd, reveal_target)
+	local command = string.format("Fern %s -drawer -reveal=%s", wd, relative_reveal_target) 
+	if is_target_directory then
+		command = string.format("Fern %s -reveal=%s", wd, relative_reveal_target)
+	end
 	vim.api.nvim_command(command)
 end
 
 return {
 	"lambdalisue/fern.vim",
 	config = function()
+		vim.g.loaded_netrwPlugin = 0
+		vim.g["fern#default_hidden"] = 1
 		vim.api.nvim_create_augroup("open-folder", { clear = true })
-		vim.api.nvim_create_autocmd("VimEnter", {
+		vim.api.nvim_create_autocmd({ "VimEnter" }, {
 			group = "open-folder",
 			nested = true,
 			callback = open_fern,
 		})
-		vim.g["fern#default_hidden"] = 1
 	end,
 	dependencies = {
 		{ "lambdalisue/nerdfont.vim" },
@@ -39,7 +71,6 @@ return {
 				})
 			end,
 		},
-		{ "lambdalisue/fern-hijack.vim" },
 		{ "lambdalisue/fern-git-status.vim" },
 		{
 			"lambdalisue/fern-renderer-nerdfont.vim",

dc3f0c6c3a4452b77f0d76828236f5e564c2f257
Yoshihiro Koyama <h2so4@mineo.jp>
2024-06-23 17:53:40 +0900

    lsp completion

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,10 +1,12 @@
-return { 
+return {
 	"williamboman/mason-lspconfig.nvim",
 	config = function()
 		require("mason-lspconfig").setup()
 		require("mason-lspconfig").setup_handlers {
 			function(server_name)
-				require("lspconfig")[server_name].setup {}
+				require("lspconfig")[server_name].setup{
+					capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				}
 			end,
 		}
 		end,
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 return {
 	'hrsh7th/nvim-cmp',
 	event = 'InsertEnter',
@@ -21,9 +27,26 @@ return {
 				["<C-n>"] = cmp.mapping.select_next_item(),
 				["<C-d>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.close(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				['<Tab>'] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							if #cmp.get_entries() == 1 then
+								cmp.confirm({ select = true })
+							else
+								cmp.select_next_item()
+							end
+						--[[ Replace with your snippet engine (see above sections on this page)
+						elseif snippy.can_expand_or_advance() then
+							snippy.expand_or_advance() ]]
+						elseif has_words_before() then
+							cmp.complete()
+							if #cmp.get_entries() == 1 then
+								cmp.confirm({ select = true })
+							end
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
@@ -32,13 +55,6 @@ return {
 				{ name = "buffer" },
 			})
 		})
-		local capabilities = require('cmp_nvim_lsp').default_capabilities()
-		require('lspconfig')['lua_ls'].setup {
-			capabilities = capabilities
-		}
-		require('lspconfig')['gopls'].setup {
-			capabilities = capabilities
-		}
 	end,
 }
 
